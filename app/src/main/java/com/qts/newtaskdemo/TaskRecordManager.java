1cbaa5172870a9033438f564cd2a52d956fad812
vito <boqin2x@163.com>
2019-07-29 16:25:25 +0800

    记录前台app及时间

--- app/src/main/java/com/qts/newtaskdemo/TaskRecordManager.java
+++ app/src/main/java/com/qts/newtaskdemo/TaskRecordManager.java
@@ -1,14 +1,26 @@
 package com.qts.newtaskdemo;
 
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
+import android.app.AppOpsManager;
+import android.app.usage.UsageEvents;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.text.TextUtils;
 
 /**
@@ -18,32 +30,88 @@ import android.text.TextUtils;
  *
  * @Version
  */
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP_MR1)
 public class TaskRecordManager {
+
+    private static final String LAST_EVENT = "mLastEvent";
+    private SortedMap<Long, UsageStats> mSortedMap = new TreeMap<>(
+            new Comparator<Long>() {
+                @Override
+                public int compare(Long aLong, Long t1) {
+                    return t1.compareTo(aLong);
+                }
+            }
+    );
 
     private TaskRecordManager() {
     }
 
-    public TaskRecordManager getInstance(){
+    public static TaskRecordManager getInstance(){
         return TaskRecordManagerHandler.instance;
     }
 
-    public void setTask(@NonNull Context context){
-        UsageStatsManager usageStatsManager = null;
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-            usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
-            if(usageStatsManager!=null){
-                Calendar calendar = Calendar.getInstance();
-                long endTime = calendar.getTimeInMillis();
-                calendar.add(Calendar.DAY_OF_WEEK, -1);
-                long startTime = calendar.getTimeInMillis();
-                List<UsageStats> usageStats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_WEEKLY, startTime, endTime);
+//性能问题，过多反射
+//    public String getCurrentName(Context context){
+//        String result = "";
+//        long lastTime = 0;
+//        if(checkOps(context)){
+//            List<UsageStats> list = getUsageStatsByDay(context);
+//            if (!list.isEmpty()) {
+//                for (UsageStats usageStats : list) {
+//                    if (usageStats.getLastTimeUsed() > lastTime) {
+//                        try {
+//                            Field field = UsageStats.class.getField("mLastEvent");
+//                            if (field.getInt(usageStats) == UsageEvents.Event.MOVE_TO_FOREGROUND) {
+//                                lastTime = usageStats.getLastTimeUsed();
+//                                result = usageStats.getPackageName();
+//                            }
+//                        } catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+//
+//                    }
+//                }
+//            }
+//        }
+//        return result;
+//    }
+
+    /**
+     * 获取当前前台的app包名
+     */
+    public String getCurrentName(Context context){
+        String result = "";
+        mSortedMap.clear();
+        if(checkOps(context)){
+            List<UsageStats> list = getUsageStatsByDay(context);
+            if (!list.isEmpty()) {
+                for (UsageStats usageStats : list) {
+                    mSortedMap.put(usageStats.getLastTimeUsed(), usageStats);
+                }
             }
         }
+        Iterator<Long> iterable = mSortedMap.keySet().iterator();
+        while (iterable.hasNext()) {
+            try {
+                UsageStats usageStats = mSortedMap.get(iterable.next());
+                if (usageStats!=null) {
+                    Field field = UsageStats.class.getField(LAST_EVENT);
+                    if (field.getInt(usageStats) == UsageEvents.Event.MOVE_TO_FOREGROUND) {
+                        result = usageStats.getPackageName();
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return result;
     }
 
     private boolean isForegroud(String packageName, Context context){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-            List<UsageStats> list = getUsageStats(context);
+            List<UsageStats> list = getUsageStatsByDay(context);
             if (!list.isEmpty() && !TextUtils.isEmpty(packageName)) {
                 return packageName.equals(list.get(0).getPackageName());
             }
@@ -51,19 +119,50 @@ public class TaskRecordManager {
         return false;
     }
 
-    private List<UsageStats> getUsageStats(Context context){
+    private List<UsageStats> getUsageStatsByWeekly(Context context){
         List<UsageStats> usageStatsList = new ArrayList<>();
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
-            UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
-            if(usageStatsManager!=null){
-                Calendar calendar = Calendar.getInstance();
-                long endTime = calendar.getTimeInMillis();
-                calendar.add(Calendar.DAY_OF_WEEK, -1);
-                long startTime = calendar.getTimeInMillis();
-                usageStatsList.addAll(usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_WEEKLY, startTime, endTime));
-            }
+        UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        if(usageStatsManager!=null){
+            Calendar calendar = Calendar.getInstance();
+            long endTime = calendar.getTimeInMillis();
+            calendar.add(Calendar.DAY_OF_WEEK, -1);
+            long startTime = calendar.getTimeInMillis();
+            usageStatsList.addAll(usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_WEEKLY, startTime, endTime));
         }
         return usageStatsList;
+    }
+
+    private List<UsageStats> getUsageStatsByDay(Context context){
+        List<UsageStats> usageStatsList = new ArrayList<>();
+        UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        if(usageStatsManager!=null){
+            Calendar calendar = Calendar.getInstance();
+            long endTime = calendar.getTimeInMillis();
+            usageStatsList.addAll(usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_BEST, endTime - 30000, endTime));
+        }
+        return usageStatsList;
+    }
+
+    /**
+     * 检测权限
+     */
+    private boolean checkOps(Context context){
+        boolean isAllowed = false;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            AppOpsManager appOpsManager = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+            int mode = appOpsManager.checkOpNoThrow("android:get_usage_stats",
+                    android.os.Process.myUid(), context.getPackageName());
+
+            if (mode == AppOpsManager.MODE_ALLOWED) {
+                isAllowed = true;
+            }else {
+                isAllowed = false;
+                Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+                intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        }
+        return isAllowed;
     }
 
     private static class TaskRecordManagerHandler{
--- app/src/main/java/com/qts/newtaskdemo/TaskRecordService.java
+++ app/src/main/java/com/qts/newtaskdemo/TaskRecordService.java
@@ -6,11 +6,15 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.IBinder;
+import android.text.TextUtils;
 
 /**
  * 小任务记录服务
@@ -21,13 +25,13 @@ import android.os.IBinder;
  */
 public class TaskRecordService extends Service {
 
+    private static final String CHANNEL_ONE_ID = TaskRecordService.class.getSimpleName();
     Map<String, Integer> mCountMap;
 
     private Timer mTimer;
 
     private TimerTask mTimerTask;
 
-    private int mCount = 0;
     private Notification.Builder mBuilder;
 
     @Override
@@ -43,26 +47,31 @@ public class TaskRecordService extends Service {
         mTimerTask = new TimerTask() {
             @Override
             public void run() {
-                mCountMap.get("");
-                mCount ++ ;
-                if (mBuilder!=null) {
-                    mBuilder.setContentText("运行"+mCount);
-                }else {
-                    mBuilder = new Notification.Builder(TaskRecordService.this.getApplicationContext());
-                    Intent nfIntent = new Intent(TaskRecordService.this, MainActivity.class);
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP_MR1) {
+                    String pn = TaskRecordManager.getInstance().getCurrentName(TaskRecordService.this);
+                    if (TextUtils.isEmpty(pn)) {
+                        return;
+                    }else {
+                        Integer count = mCountMap.get(pn);
+                        if (count == null) {
+                            count = 0;
+                        }
+                        count ++ ;
+                        mCountMap.put(pn, count);
 
-                    mBuilder.setContentIntent(PendingIntent.
-                            getActivity(TaskRecordService.this, 0, nfIntent, 0)) // 设置PendingIntent
-                            .setLargeIcon(BitmapFactory.decodeResource(TaskRecordService.this.getResources(),
-                                    R.mipmap.ic_launcher)) // 设置下拉列表中的图标(大图标)
-                            .setContentTitle("下拉列表中的Title") // 设置下拉列表里的标题
-                            .setSmallIcon(R.mipmap.ic_launcher) // 设置状态栏内的小图标
-                            .setContentText("运行"+mCount) // 设置上下文内容
-                            .setWhen(System.currentTimeMillis()); // 设置该通知发生的时间
+                        if (mBuilder==null) {
+                            mBuilder = initBuilder();
+                        }
+
+                        mBuilder.setContentTitle(pn) // 设置下拉列表里的标题
+                                .setContentText("运行"+count);
+                        Notification notification = mBuilder.build();
+                        notification.defaults = Notification.DEFAULT_SOUND;
+                        startForeground(101, notification);
+                    }
+
                 }
-                Notification notification = mBuilder.build();
-                notification.defaults = Notification.DEFAULT_SOUND;
-                startForeground(101, notification);
+
             }
         };
     }
@@ -82,5 +91,33 @@ public class TaskRecordService extends Service {
         }
 
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    /**
+     * 初始化Notification的Builder，需要处理8.0+的channel配置
+     */
+    private Notification.Builder initBuilder(){
+        Notification.Builder builder = new Notification.Builder(TaskRecordService.this.getApplicationContext());
+        Intent nfIntent = new Intent(TaskRecordService.this, MainActivity.class);
+
+        builder.setContentIntent(PendingIntent.
+                getActivity(TaskRecordService.this, 0, nfIntent, 0)) // 设置PendingIntent
+                .setLargeIcon(BitmapFactory.decodeResource(TaskRecordService.this.getResources(),
+                        R.mipmap.ic_launcher)) // 设置下拉列表中的图标(大图标)
+                .setSmallIcon(R.mipmap.ic_launcher) // 设置状态栏内的小图标
+                .setWhen(System.currentTimeMillis()); // 设置该通知发生的时间
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel= new NotificationChannel(CHANNEL_ONE_ID,
+                    CHANNEL_ONE_ID, NotificationManager.IMPORTANCE_LOW);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setShowBadge(true);
+            notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+            NotificationManager manager= (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(notificationChannel);
+            builder.setChannelId(CHANNEL_ONE_ID);
+        }
+
+        return builder;
     }
 }
